ea8b905d4cfbcc0d8a9fdc952ae33a81eaf7458c
cristinarv <c.delriove@alumnos.urjc.es>
2018-04-16 20:37:38 +0200

    creado version 1

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,11 @@
+# Cristina del Río 
 from django.shortcuts import render
+from cms.models import Pages
 
 # Create your views here.
+def inicio_pag(request):
+    resp = "<u><h4>La lista de las paginas es:</h4></u>"
+    list_pags = Pages.objects.all()
+    for pag in list_pags:
+        resp += "<li><a href=" + str(pag.id) + ">" +  pag.name +  "</a></li>"
+    return HttpResponse(resp)
