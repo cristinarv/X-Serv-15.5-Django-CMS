37b6a581c80e84b6e8914551f824098ea59c41a6
cristinarv <c.delriove@alumnos.urjc.es>
2018-04-16 20:54:19 +0200

    version 2 finalizada

--- cms/views.py
+++ cms/views.py
@@ -1,16 +1,19 @@
 # Cristina del Río 
 from django.shortcuts import render
 from cms.models import Pages
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 def inicio_pag(request):
-    resp = "<u><h4>La lista de las paginas es:</h4></u>"
+    resp = "<u><h4>La lista de las paginas es:</h4></u><br>"
     list_pags = Pages.objects.all()
     for pag in list_pags:
-        resp += "<li><a href=" + str(pag.id) + ">" +  pag.name +  "</a></li>"
+        resp += "<li><a href=" + str(pag.id) + ">" +  pag.name +  "</a></li><br>"
     return HttpResponse(resp)
 def pag(request, id):
-    pag = Pages.objects.get(id=id)
-    resp = pag.name + pag.page
-    return HttpResponse(resp)
+    try:
+        pag = Pages.objects.get(id=id)
+        resp = "El nombre es: " + pag.name + " <br>Su página es: " + pag.page
+        return HttpResponse(resp)
+    except Pages.DoesNotExist:
+        return HttpResponseNotFound("<h2>La página introducida no existe</h2>")
